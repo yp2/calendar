b53d9710a97535d11f3634ceef25eda6dacff16d
Daniel 'yp2' Derezinski <daniel.derezinski@gmail.com>
2015-04-22 22:41:08 +0200

    formularze

--- apps/home/client/home.js
+++ apps/home/client/home.js
@@ -19,6 +19,39 @@ Template.home.helpers({
     }
 });
 
+Template.home.onRendered(function(){
+    var today = moment();
+    this.$('#pickerDateFrom').datetimepicker({
+        locale:"pl",
+        format: "dddd, D MMMM YYYY",
+        minDate: today
+    });
+    this.$('#pickerTimeFrom').datetimepicker({
+        locale:"pl",
+        format: "HH:00",
+        minDate: today,
+        maxDate: moment({hour: 23, minute: 59})
+    });
+
+    this.$('#pickerDateFrom').on('dp.change', function(e){
+        var today = moment().format('DD-MM-YYYY');
+        var eventDate = e.date.format('DD-MM-YYYY');
+
+        var pickerTimeFrom = $('#pickerTimeFrom');
+        var timeFrom = $("#timeFrom");
+        if (today !== eventDate){
+            pickerTimeFrom.data("DateTimePicker").maxDate(moment({hour: 23, minute: 59}));
+            pickerTimeFrom.data("DateTimePicker").minDate(moment({hour: 0, minute: 0}));
+        } else {
+            pickerTimeFrom.data("DateTimePicker").maxDate(moment({hour: 23, minute: 59}));
+            pickerTimeFrom.data("DateTimePicker").minDate(moment());
+            if(timeFrom.val()){
+                    pickerTimeFrom.data("DateTimePicker").date(moment());
+            }
+        }
+    })
+});
+
 Template.home.events({
     'click .calNavPrev' : function (e){
         e.preventDefault();
@@ -31,5 +64,13 @@ Template.home.events({
         var currentDate = moment(Session.get('dateShow'));
         var date = currentDate.add(1, 'days');
         Session.set('dateShow', date.toISOString());
+    },
+    'blur #dateFrom': function (e, t){
+        //console.log(e, t);
+        var min = t.$("#dateFrom").val();
+        min = moment(min, "dddd, D MMMM YYYY");
+        //console.log(t.$('#pickerTimeFrom').data('DateTimePicker').minDate())
+
+
     }
 });
